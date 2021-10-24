3048f85b4ef298ce759dbc5a5cf082f26234a6e4
bushrabarakat1 <Bushra.Barakat.jiz.s@tuwaiq.edu.sa>
2021-10-24 11:00:25 +0300

    Bushra2

--- BushraBarakt Lab12/BushraBarakt Lab12/ViewController.swift
+++ BushraBarakt Lab12/BushraBarakt Lab12/ViewController.swift
@@ -82,70 +82,94 @@ struct Power {
 
 class ViewController: UIViewController {
 
-    var lazarus = HeroLazarus.init(name: "Lazarus", characteristic: "knight", level: 10, lifePoint: 160, heal: 10, swordAttack: -10, megaFist: -30)
+    var lazarus = HeroLazarus.init(name: "Lazarus", characteristic: "knight", level: 10, lifePoint: 160, heal: 10, swordAttack: 10, megaFist: 30)
     
-    var elvin = HeroElvin.init(name: "Elvin", characteristic: "wizard", level: 10, lifePoint: 140, heal: 10, stickOfTheWise: -15, fireBall: -20)
-    
-    
-    var lazarusWeapon = Weapon.init(name: "sword attack", damage: -10, attackAction: "Lazarus uses his powerfull sword to his opponent - 10 Damage")
-    
-    var elvinWeapon = Weapon.init(name: "stick of the wise", damage: -15, attackAction: "Elvin uses his magical and strange stick to beat his opponent - 15 Damage ")
+    var elvin = HeroElvin.init(name: "Elvin", characteristic: "wizard", level: 10, lifePoint: 140, heal: 10, stickOfTheWise: 15, fireBall: 20)
     
     
-    var lazarusPower = Power.init(name: "mega fist", damage: -30, attackAction: "Lazarus uses his Fist to beat his opponent - 30 Damage and git +2 Life Point", specialEffect: 2)
+    var lazarusWeapon = Weapon.init(name: "sword attack", damage: 10, attackAction: "Lazarus uses his powerfull sword to his opponent - 10 Damage")
     
-    var elvinPower = Power.init(name: "fire ball", damage: -20, attackAction: "Elvin uses the powerful fire spell learned centuries ago - 20 Damage and empower his his by 10", specialEffect: 10)
+    var elvinWeapon = Weapon.init(name: "stick of the wise", damage: 15, attackAction: "Elvin uses his magical and strange stick to beat his opponent - 15 Damage ")
+    
+    
+    var lazarusPower = Power.init(name: "mega fist", damage: 30, attackAction: "Lazarus uses his Fist to beat his opponent - 30 Damage and git +2 Life Point", specialEffect: 2)
+    
+    var elvinPower = Power.init(name: "fire ball", damage: 20, attackAction: "Elvin uses the powerful fire spell learned centuries ago - 20 Damage and empower his his by 10", specialEffect: 10)
     
     var dead = 0
-
+    
+    @IBOutlet weak var heroImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 }
 
     @IBAction func button(_ sender: Any) {
-        while lazarus.lifePoint > dead && elvin.lifePoint > dead {
+        if lazarus.lifePoint == dead || elvin.lifePoint == dead { print ("end of the game")}
+        
+        if  lazarus.lifePoint > dead || elvin.lifePoint > dead {
             let randomDice = Int.random(in:1...6)
             if randomDice == 1 {
                 print ("the dice roll is \(randomDice)")
                 lazarus.healLazarus()
                 lazarus.lifePoint += lazarus.heal
-                print ("Lazarus have \(lazarus.lifePoint) life point")
+                print ("Lazarus life point =\(lazarus.lifePoint) ")
+                print ("elvin life point = \(elvin.lifePoint)")
+                print ("...................................")
             }else if randomDice == 2 {
                 print ("the dice roll is \(randomDice)")
                 lazarusWeapon.swordAction()
-                elvin.lifePoint += lazarusWeapon.damage
-                lazarus.lifePoint += lazarusPower.specialEffect
-                print ("elvin have \(elvin.lifePoint) life point")
-                print ("lazarus have \(lazarus.lifePoint) life point")
+                elvin.lifePoint -= lazarusWeapon.damage
+                print ("elvin life point = \(elvin.lifePoint) ")
+                print ("lazarus life point = \(lazarus.lifePoint) ")
+                print ("...................................")
             }else if randomDice == 3 {
                 print ("the dice roll is \(randomDice)")
                 lazarusPower.megaFist()
-                elvin.lifePoint += lazarusPower.damage
-                print ("elvin have \(elvin.lifePoint) life point")
+                elvin.lifePoint -= lazarusPower.damage
+                lazarus.lifePoint += lazarusPower.specialEffect
+                print ("elvin life point = \(elvin.lifePoint)")
+                print ("lazarus life point = \(lazarus.lifePoint) ")
+                print(".....................................")
             }else if randomDice == 4 {
                 print ("the dice roll is \(randomDice)")
                 elvin.healElvin()
                 elvin.lifePoint += elvin.heal
-                print ("elvin have \(elvin.lifePoint) life point")
+                print ("elvin life point = \(elvin.lifePoint) ")
+                print ("lazarus life point = \(lazarus.lifePoint)")
+                print ("....................................")
             }else if randomDice == 5 {
                 print ("the dice roll is \(randomDice)")
                 elvinPower.fireBall()
-                lazarus.lifePoint -= 30//اراجع هذي النقطة
-                print ("lazarus have \(lazarus.lifePoint) life point")
+                lazarus.lifePoint -= elvinPower.damage
+                elvin.lifePoint += elvinPower.specialEffect
+                print ("elvin life point = \(elvin.lifePoint)")
+                print ("lazarus life point = \(lazarus.lifePoint)")
+                print (".....................................")
             }else {
                 print ("the dice roll is \(randomDice)")
                 elvinWeapon.stickAction()
-                lazarus.lifePoint += elvinWeapon.damage
-                print ("lazarus have \(lazarus.lifePoint) life point")
+                lazarus.lifePoint -= elvinWeapon.damage
+                print ("lazarus life point = \(lazarus.lifePoint) ")
+                print ("elvin life point = \(elvin.lifePoint) ")
+                print ("......................................")
             }
             if elvin.lifePoint <= dead {
                 print ("\(lazarus.name) is a WINNER 🥳")
+                heroImage.image = UIImage (named:  "Lazarus")
+                
+                
             }else if lazarus.lifePoint <= dead {
                 print ("\(elvin.name) is a WINNER 🥳")
+                heroImage.image = UIImage (named: "Elvin")
+                
+                print ("......................................")
             }
         }
-        print ("end of the game")
+   
+         
+        
     }
     
 
