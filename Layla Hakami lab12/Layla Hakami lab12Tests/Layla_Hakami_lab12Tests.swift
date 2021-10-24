442106437ed136f18d68569c2b8a8f49f26ca890
layla hakami <laylahakami@laylas-MacBook-Air.local>
2021-10-24 08:37:29 +0300

    layla

---
+++ Layla Hakami lab12/Layla Hakami lab12Tests/Layla_Hakami_lab12Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Layla_Hakami_lab12Tests.swift
+//  Layla Hakami lab12Tests
+//
+//  Created by layla hakami on 17/03/1443 AH.
+//
+
+import XCTest
+@testable import Layla_Hakami_lab12
+
+class Layla_Hakami_lab12Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
