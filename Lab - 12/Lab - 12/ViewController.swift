6f5173f9394c8121aa40622a70e58d5c8eb5a52c
Afrah Omar <afrahomar@MacBook-Pro-Afrah.local>
2021-10-25 02:13:02 +0300

    done

--- Lab - 12/Lab - 12/ViewController.swift
+++ Lab - 12/Lab - 12/ViewController.swift
@@ -6,8 +6,9 @@ class Hero {
     var name: String
     var level: Int
     var lifePoint: Int
+    var charact: String
     
-    //The heal//
+    // The heal//
     
     struct Heal {
         var healingAmount: Int
@@ -34,14 +35,15 @@ class Hero {
         }
     }
     
-    init(_ name: String, level: Int, lifePoint: Int) {
+    init(_ name: String, level: Int, lifePoint: Int, charact: String) {
         self.name                    = name
         self.level                   = level
-        self.lifePoint          = lifePoint
+        self.lifePoint        = lifePoint
+        self.charact          = charact
     }
 }
 
-                            //Lazarus skills //
+                            //....Lazarus skills... //
 
 class Lazarus: Hero {
     var heal = Heal(healingAmount: 10)
@@ -50,21 +52,24 @@ class Lazarus: Hero {
 }
 
 
-                           //Elvin skills//
+                           //.....Elvin skills....//
 
 class Elvin: Hero {
     var heal = Heal(healingAmount: 15)
     var weapon = Weapon(nameOfAttack: "Stick of the wise", damageOfAttack: 15)
     var power = Power(name: "Fire Ball", damage: 20, specialEffect: 10)
 }
-//___________________________//
-//Lazarus got 3 special skills - Life point : 160//
-//Elvin got 3 special skills - Life point 140//
 
+                         //___________________________//
+            //..Lazarus got 3 special skills - Life point : 160..//
+              //..Elvin got 3 special skills - Life point 140..//
 class ViewController: UIViewController {
-    var liz = Lazarus("Lazarus", level: 0, lifePoint: 160)
-    
-    var el  = Elvin("Elvin", level: 0, lifePoint: 140)
+    //    ____________
+    var turn: Int = 0
+    var winner = false
+    //    ____________
+    var liz = Lazarus("Lazarus", level: 0, lifePoint: 160, charact: "Knight")
+    var el  = Elvin("Elvin", level: 0, lifePoint: 140, charact: "Wizard")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,7 +77,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rest: UIButton!
     
+    // Start Game Lazarus VS Elvin 🎲🎲 ....
+  
     @IBAction func dicrull(_ sender: Any) {
+        
             let number = Int.random(in: 1...6)
             if number <= 3 {
                 print(liz.name + " turn!")
@@ -83,13 +91,13 @@ class ViewController: UIViewController {
 
                 el.lifePoint -= liz.weapon.damageOfAttack
 
-                print("lizarus uses his \(liz.weapon.nameOfAttack)")
-                print("elvis health now \(el.lifePoint)")
+                print("lizarus uses his 🗡⚔️ \(liz.weapon.nameOfAttack)")
+                print("elvis health now 💉🩸 \(el.lifePoint)")
 
                 el.lifePoint -= liz.power.damage
                 liz.lifePoint = liz.lifePoint + liz.power.specialEffect
-                print("lizarus uses his \(liz.power.name)")
-                print("elvis health now \(el.lifePoint)")
+                print("lizarus uses his 👊👊 \(liz.power.name)")
+                print("elvis health now 💉🩸 \(el.lifePoint)")
             }else{
                 print(el.name + " turn!")
                 
@@ -99,14 +107,30 @@ class ViewController: UIViewController {
                 
                 liz.lifePoint -= el.weapon.damageOfAttack
                 
-                print("elvin uses his \(el.weapon.nameOfAttack)")
-                print("lizarus health now \(liz.lifePoint)")
+                print("elvin uses his  🪄🪄 \(el.weapon.nameOfAttack)")
+                print("lizarus health now 💉🩸 \(liz.lifePoint)")
                 
                 liz.lifePoint -= el.power.damage + el.power.specialEffect
                 
-                print("elvin uses his \(el.power.name)")
-                print("lizarus health now \(liz.lifePoint)")
-            }
+                print("elvin uses his 💥💥 \(el.power.name)")
+                print("lizarus health now 💉🩸 \(liz.lifePoint)")
+                
+          
+    // Finally ..  Whose Winner Of The Game Lazarus OR Elvin ...?!!
+    
+    theWinnerIs(hero1: liz, hero2: el)
+                
+func theWinnerIs(hero1: Hero, hero2: Hero) {
+    
+    if (liz.lifePoint <= 0) {
+        print("The Winner is  🎉🎉👏 : \(el.name) at turn \(turn)")
+        winner = true
+    }
+    if (el.lifePoint <= 0) {
+        print("The winner is  🎉🎉👏 : \(liz.name) at turn \(turn)")
+        winner = true
+    }
+}
             
             
             if liz.lifePoint <= 0 {
@@ -116,5 +140,9 @@ class ViewController: UIViewController {
                 rest.isEnabled = false
                 print("lizarus won")
             }
+            }
     }
 }
+
+// GAME OVER ...
+
