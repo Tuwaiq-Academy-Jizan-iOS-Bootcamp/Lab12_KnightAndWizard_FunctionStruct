27f1d068f9a95d1fe45ab36322d9c85e2eaa18d6
Arwa Alattas <arwaalattas@MacBook-Pro-Arwa.local>
2021-10-25 16:04:32 +0300

    ArwaAlattas_lab12-knightAndWizard-last  update

--- ArwaAlattas_lLab12 KnightAndWizard ClassStruct/ArwaAlattas_lLab12 KnightAndWizard ClassStruct/ViewController.swift
+++ ArwaAlattas_lLab12 KnightAndWizard ClassStruct/ArwaAlattas_lLab12 KnightAndWizard ClassStruct/ViewController.swift
@@ -29,7 +29,12 @@ class Lazarus : Hero {
         super.init(name: name, characteristic: characteristic, level:level, lifePoint: lifePoint)
        
     }
-
+    func restOfLife() {
+        if lifePoint < 0 {
+            lifePoint = 0
+        }
+        print("Rest of life point for Lazarus : \(lifePoint)")
+    }
 }
                                    // ELVIN \\
 
@@ -39,7 +44,13 @@ class Elvin : Hero {
         super.init(name: name, characteristic: characteristic, level:level, lifePoint: lifePoint)
         
     }
-   
+    func restOfLife() {
+        if lifePoint < 0 {
+            lifePoint = 0
+        }
+        print("Rest of life for Elvin : \(lifePoint)")
+    }
+    
 }
 struct Weapon {
 
@@ -62,6 +73,7 @@ class ViewController: UIViewController {
   
     @IBOutlet weak var rollthedise: UIButton!
 
+    var winner = false
     var turnNumber = 0
     
     override func viewDidLoad() {
@@ -79,70 +91,71 @@ class ViewController: UIViewController {
   
     @IBAction func rollTheDice(_ sender: UIButton) {
       
+        if winner {
+            print("Game Over ! ")
+        } else {
         
-      if lazarus.lifePoint > 0 && elvin.lifePoint > 0 {
              turnNumber += 1
             let rollofdice = Int.random(in: 1...6)
-           if rollofdice == 1 {
+            switch rollofdice {
+            case  1 :
             
             print ("-------------------------Turn Number \(turnNumber)-----------------------------------")
             print("The Dice = \(rollofdice)")
             print("Lazaruse use a Heal that + 10 to hid life point")
                lazarus.lifePoint += lazarus.heal
-            print("life point for Lazaruse is \(lazarus.lifePoint)  ")
-            print("life point for Elvin is \(elvin.lifePoint)")
-              
-           }else if rollofdice == 2{
+                lazarus.restOfLife()
+                elvin.restOfLife()
+            case 2:
             print ("-------------------------Turn Number \(turnNumber)-----------------------------------")
             print("The Dice = \(rollofdice)")
             print("\(lazarus.name)use \(sword.attackAction)damage to \(elvin.name) ")
             elvin.lifePoint -= sword.damage
-           print("life point for Lazaruse is \(lazarus.lifePoint)  ")
-           print("life point for Elvin is \(elvin.lifePoint)")
-               }
-        else if rollofdice  == 3 {
+                lazarus.restOfLife()
+                elvin.restOfLife()
+            case 3 :
            print ("-------------------------Turn Number \(turnNumber)-----------------------------------")
            print("The Dice = \(rollofdice)")
            print("\(lazarus.name)use \(mega.attackAction)damage to \(elvin.name) and it is have special effect it add to lazarus 2 life point ")
            elvin.lifePoint -= mega.damage
            lazarus.lifePoint += mega.specialEffect
-            print("life point for Lazaruse is \(lazarus.lifePoint) ")
-            print("life point for Elvin is \(elvin.lifePoint)")
-        }
-        else if rollofdice == 4 {
+                lazarus.restOfLife()
+                elvin.restOfLife()
+            case 4 :
             print ("-------------------------Turn Number \(turnNumber)-----------------------------------")
             print("The Dice = \(rollofdice)")
             print("Elvine use a Heal that + 15 to hid life point")
                elvin.lifePoint += elvin.heal
-            print("life point for Lazaruse is \(lazarus.lifePoint)  ")
-            print("life point for Elvin is \(elvin.lifePoint)")
-            
-        }else if rollofdice == 5 {
+                lazarus.restOfLife()
+                elvin.restOfLife()
+            case 5 :
             print ("-------------------------Turn Number \(turnNumber)-----------------------------------")
             print("The Dice = \(rollofdice)")
             print("\(elvin.name)use \(fireBall.attackAction)damage to \(lazarus.name) and it is have special effect it subtract 10 life point for lazarus")
             lazarus.lifePoint -= fireBall.damage
             lazarus.lifePoint -= fireBall.specialEffect
-            print( "life point for Lazaruse is \(lazarus.lifePoint)  ")
-           print("life point for Elvin is \(elvin.lifePoint)")
-
-        }else if rollofdice == 6{
+                lazarus.restOfLife()
+                elvin.restOfLife()
+            case 6 :
            print ("-------------------------Turn Number \(turnNumber)-----------------------------------")
            print("The Dice = \(rollofdice)")
            print("\(elvin.name)use \(stick.attackAction)damage to \(lazarus.name) ")
            lazarus.lifePoint -= stick.damage
-            print( "life point for Lazaruse is \(lazarus.lifePoint)  ")
-            print("life point for Elvin is \(elvin.lifePoint)")
+                lazarus.restOfLife()
+                elvin.restOfLife()
+            default :
+                print("Error")
+                
        }
+
         }
-        
         if lazarus.lifePoint <= 0 {
-            
            print("🥳🥳the winner is Elvin 🥳🥳")
-            rollthedise.isEnabled = false
+            winner = true
+    
         }else if elvin.lifePoint <= 0  {
             print("🥳🥳the winner is Lazarus🥳🥳")
-            rollthedise.isEnabled = false
+            winner = true
         }}
        
 }
