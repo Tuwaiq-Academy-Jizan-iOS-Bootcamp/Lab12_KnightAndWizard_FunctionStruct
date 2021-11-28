224dfcc4a833dd2d6fb330167dbe01869aa58ea8
Ehab hakami <ehab7k@gmail.com>
2021-11-28 13:49:47 +0300

    ehan12

---
+++ diceGame/newdicegameeUITests/newdicegameeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  newdicegameeUITestsLaunchTests.swift
+//  newdicegameeUITests
+//
+//  Created by Ehab Hakami on 18/03/1443 AH.
+//
+
+import XCTest
+
+class newdicegameeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
